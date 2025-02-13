5e427411e538d283835c28e9aba8ba2674bf1d5d
pranav <pranvkp44@gmail.com>
2025-02-13 19:59:00 +0530

    commit13

--- logicTry3.js
+++ logicTry3.js
@@ -31,4 +31,55 @@ const sortArray = (arr) => {
 const findDuplicate = (arr) => {
   console.log([...new Set(arr)]);
 };
-findDuplicate([1, 2, 2, 3, 3, 3, 4]);
+// findDuplicate([1, 2, 2, 3, 3, 3, 4]);
+
+// 17
+
+const camelcaseCon = (arr) => {
+  arr = arr.split(" ");
+  let word = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      word.push(arr[i].toLowerCase());
+    } else {
+      let cap = arr[i].charAt(0).toUpperCase();
+      let rest = cap + arr[i].slice(1);
+      word.push(rest);
+    }
+  }
+  console.log(word.join(""));
+};
+// camelcaseCon("hello world mern");
+
+// 23
+
+const repitationCheck = (arr) => {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!obj[arr[i]]) {
+      obj[arr[i]] = 1;
+    } else {
+      obj[arr[i]] += 1;
+    }
+  }
+  console.log(obj);
+
+  let mode = 0;
+  for (let key in obj) {
+    if (obj[key] > mode) {
+      mode = key;
+      // console.log(obj[key], key, mode);
+    }
+  }
+  console.log(mode);
+};
+
+// repitationCheck([1, 2, 2, 3, 3, 1, 2, 4]);
+
+let arry1 = [1, 2, 2, 3, 3, 4];
+
+const dup = (arr) => {
+  console.log(...new Set(arr));
+};
+
+dup(arry1);
